b9f3f3bd5934e988c18ef54318b18d5667c0216b
Andres Alban <bethesdas101@gmail.com>
2020-10-28 10:38:22 -0500

    Updating main component styles and setting up the team sign ins

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { TournametService } from 'src/app/services/tournamet.service';
 
 @Component({
@@ -7,16 +9,19 @@ import { TournametService } from 'src/app/services/tournamet.service';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  team1Name: string;
-  team2Name: string;
+  @ViewChild('input1') team1Name: ElementRef;
+  @ViewChild('input2') team2Name: ElementRef;
 
-  constructor(private tournametService: TournametService) { }
+  constructor(private tournametService: TournametService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   public play(): void {
-
+    const team1 = this.team1Name.nativeElement.value;
+    const team2 = this.team2Name.nativeElement.value;
+    this.tournametService.setMatch(team1, team2);
+    this.router.navigateByUrl('/marcador');
   }
 
 }
