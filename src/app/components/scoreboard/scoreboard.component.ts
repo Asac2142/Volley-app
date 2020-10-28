01db6a563210c2ff58d591fd3fa6811c475b28dd
Andres Alban <bethesdas101@gmail.com>
2020-10-28 12:25:43 -0500

    Adding setWinner function, adding styles for component, updating html template

--- src/app/components/scoreboard/scoreboard.component.ts
+++ src/app/components/scoreboard/scoreboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 import { TournametService } from 'src/app/services/tournamet.service';
 
@@ -8,24 +8,58 @@ import { TournametService } from 'src/app/services/tournamet.service';
   styleUrls: ['./scoreboard.component.scss']
 })
 export class ScoreboardComponent implements OnInit {
-  @Input('lastMatch') match: {team1: string, score: string[], team2: string, winner: string};
+  @Input('lastMatch') match: {team1: string, score: string[], team2: string, accountant: { team1: number, team2: number}};
+  @Input('gameSet') gameSet: string;
+  @ViewChild('score1') score1: ElementRef;
+  @ViewChild('score2') score2: ElementRef;
+
   public checkForService = false;
 
   constructor(private tournamentService: TournametService) { }
 
-  ngOnInit(): void { 
-  }
+  ngOnInit(): void { }
 
-  public setService(): void {
-    this.checkForService = !this.checkForService;
-  }
+  public addScoreTeam1(): void {    
+    let score1 = parseInt(this.score1.nativeElement.value);
+    const score2 = parseInt(this.score2.nativeElement.value);    
+    score1++;
 
-  public addScoreTeam1(): void {
-    
+    this.setWinner(score1, score2, this.match.team1);
+    this.score1.nativeElement.value = score1;
+    this.setService();    
   }
 
   public addScoreTeam2(): void {
-    
+    let score2 = parseInt(this.score2.nativeElement.value);
+    const score1 = parseInt(this.score1.nativeElement.value);        
+    score2++;
+
+    this.setWinner(score2, score1, this.match.team2);
+    this.score2.nativeElement.value = score2;
+    this.setService();
+  }
+
+  private setWinner(score1: number, score2: number, team: string): void {
+    const gmSt = parseInt(this.gameSet);
+    const difference = score1 - score2;
+
+    if (gmSt >= 1 && gmSt <= 4) {
+      if (score1 >= 25 && difference >= 0) {
+        if (difference >= 2) {
+          console.log(`${team} gana el set #${gmSt}`);
+        }
+      }
+    } else {        
+        if (score1 >= 15 && difference >= 0) {
+          if (difference >= 2) {
+            console.log(`${team} gana el set ${gmSt}`);
+          }
+        }
+    }
+  }
+
+  private setService(): void {
+    this.checkForService = !this.checkForService;
   }
 
 }
