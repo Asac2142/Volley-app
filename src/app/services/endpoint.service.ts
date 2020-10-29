35b0686ec83aa9c6f42541c1a0e374cc0e40738e
Andres2142 <bethesdas101@gmail.com>
2020-10-29 03:04:38 -0500

    Updating endpoint service for sending payload to server

--- src/app/services/endpoint.service.ts
+++ src/app/services/endpoint.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { TournametService } from './tournamet.service';
@@ -7,11 +7,18 @@ import { TournametService } from './tournamet.service';
   providedIn: 'root'
 })
 export class EndpointService {
-  private URL = '...';
+  private URL = 'https://localhost:44368/creatematch/';
+  private header = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient, private tournamentService: TournametService) { }
 
   public sendMatch(): void {
-    this.http.post(this.URL, this.tournamentService.getLastTournament());
+    const lastMatch = JSON.stringify(this.tournamentService.getLastTournament());
+
+    this.http.post(this.URL, lastMatch, { headers: this.header })
+      .subscribe(
+        (response) => console.log('Response from server:', response),
+        (error) => console.log('Error message', error)
+      );
   }
 }
